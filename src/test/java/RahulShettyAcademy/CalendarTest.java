e7a5117abb26b9d85bc533c762eefbbd9694a83d
7903090370 <kamalesh1992rai@gmail.com>
2025-12-09 22:24:20 +0530

    new code

--- src/test/java/RahulShettyAcademy/CalendarTest.java
+++ src/test/java/RahulShettyAcademy/CalendarTest.java
@@ -1,6 +1,21 @@
 package RahulShettyAcademy;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class CalendarTest {
 
+    public static void manin(String[] args) {
 
+
+        String monthNumber = "6";
+        String date = "15";
+        String year = "2027";
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://rahulshettyacademy.com/seleniumPractise/#/offers");
+
+
+    }
 }
